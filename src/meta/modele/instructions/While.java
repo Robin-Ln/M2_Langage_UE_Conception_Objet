a99a727026f48a13f62dde48abcd13f305bf9f32
Robin Louarn <robin.louarn@icloud.com>
2018-11-12 21:14:47 +0100

    pom.xml

---
+++ src/meta/modele/instructions/While.java
@@ -0,0 +1,64 @@
+package meta.modele.instructions;
+
+import exceptions.PropagationExeption;
+import meta.modele.block.Block;
+import meta.modele.expression.Expression;
+import visiteur.Visiteur;
+
+public class While extends Instruction {
+
+	/*
+	 * Attributs
+	 */
+
+	private String idExpressionCondition;
+	private String idBlock;
+
+	private Expression expressionCondition;
+	private Block block;
+
+	/*
+	 * Methodes
+	 */
+
+	@Override
+	public void accept(Visiteur visiteur) throws PropagationExeption {
+		visiteur.visite(this);
+	}
+
+	/*
+	 * Accesseurs
+	 */
+	public String getIdExpressionCondition() {
+		return idExpressionCondition;
+	}
+
+	public void setIdExpressionCondition(String idExpressionCondition) {
+		this.idExpressionCondition = idExpressionCondition;
+	}
+
+	public String getIdBlock() {
+		return idBlock;
+	}
+
+	public void setIdBlock(String idBlock) {
+		this.idBlock = idBlock;
+	}
+
+	public Expression getExpressionCondition() {
+		return expressionCondition;
+	}
+
+	public void setExpressionCondition(Expression expressionCondition) {
+		this.expressionCondition = expressionCondition;
+	}
+
+	public Block getBlock() {
+		return block;
+	}
+
+	public void setBlock(Block block) {
+		this.block = block;
+	}
+
+}
